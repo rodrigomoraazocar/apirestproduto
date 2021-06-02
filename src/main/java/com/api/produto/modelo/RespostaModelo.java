71af223a8ba889721df6ced8ad40c98704ae5f32
Rodrigo A. Mora Azócar <brazuk25@gmail.com>
2021-06-01 21:41:53 -0300

    Realización Completa del Crud

---
+++ src/main/java/com/api/produto/modelo/RespostaModelo.java
@@ -0,0 +1,23 @@
+package com.api.produto.modelo;
+
+public class RespostaModelo {
+	
+	
+	//Atributo
+	
+	private String mensagem;
+
+	
+	
+	//Get and Setter
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+	
+
+}
